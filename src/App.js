d73909080842ab886c5d0b2eda79e4712b07fdfe
Alinka08 <alinazoldubaeva@gmail.com>
2023-06-05 16:42:36 -0500

    orgonizing app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Todoinput from "./components/todo-input/Todo-input";
-import GutterlessList from "./components/todo-input/list-group/List-group";
+// import Todoinput from "./components/todo-input/Todo-input";
+// import GutterlessList from "./components/todo-input/list-group/List-group";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Todoinput />
-      <GutterlessList />
+      {/* <Todoinput /> */}
+      {/* <GutterlessList /> */}
     </div>
   );
 }
