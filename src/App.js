e6379751c45ee07152cfa408871521d2226eda6f
Alinka08 <alinazoldubaeva@gmail.com>
2023-06-05 16:47:38 -0500

    small changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import Todoinput from "./components/todo-input/Todo-input";
 // import GutterlessList from "./components/todo-input/list-group/List-group";
 import "./App.css";
+import ListGroup from "./list-group/List-group";
 
 function App() {
   return (
     <div className="App">
       {/* <Todoinput /> */}
-      {/* <GutterlessList /> */}
+      <ListGroup />
     </div>
   );
 }
--- src/list-group/List-group.jsx
+++ src/list-group/List-group.jsx
@@ -1,23 +1,23 @@
-// import "./list-group.style.css";
-// import * as React from "react";
-// import { useState } from "react";
+import "./list-group.style.css";
+import * as React from "react";
+import { useState } from "react";
 
-// export default function ListGroup() {
-//   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+export default function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-//   const handleClick = (event) => console.log(event);
+  const handleClick = (event) => console.log(event);
 
-//   return (
-//     <>
-//       <h1>List</h1>
-//       {/* {items.length === 0 && <p>No item found</p>} */}
-//       <ul className="list-group">
-//         {items.map((item, index) => (
-//           <li className="list-group-item" key={item} onClick={handleClick}>
-//             {item}
-//           </li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <h1>List</h1>
+      {/* {items.length === 0 && <p>No item found</p>} */}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
